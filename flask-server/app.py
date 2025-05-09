e7985a94988bb062b003c6596788d760b8b1f03e
Puja Shah <pks37@sfu.ca>
2025-05-09 02:33:41 -0700

    requirements updated

--- flask-server/app.py
+++ flask-server/app.py
@@ -5,7 +5,7 @@ import subprocess
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 UPLOAD_FOLDER = 'uploads'
 PROCESSED_FOLDER = 'processed'
